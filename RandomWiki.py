ce1649b6327b5e437ce0e5cdd240c922ab0c10eb
Antoni Mleczko <antonimleczko@icloud.com>
2026-08-11 23:54:07 +0200

    Simple API. It took me like 1h its really simple and i guess ready. It dont need nothing more

---
+++ RandomWiki.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from wonderwords import RandomWord
+
+w = RandomWord()
+app = FastAPI()
+
+@app.get("/")
+def main():
+    return {"IDK what to write here": "."}
+
+@app.get("/link")
+def link():
+    word = w.word(include_parts_of_speech=["nouns"])
+    wiki_link = f"https://wikipedia.org/wiki/{word}"
+
+    return {"word": word, "link": wiki_link}
